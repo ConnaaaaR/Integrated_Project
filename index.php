73cbfa2a3ba8c69e53b050d38eb5befa2e1ddb3f
ConnaaaR <connormattless@gmail.com>
2022-03-17 00:33:18 +0000

    icon

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
 <?php
+    require_once 'classes/DBConnector.php';
     //----------------------------------
     // retreive stories
     //----------------------------------
-    
-    require_once 'classes/DBConnector.php';
-     // small side stories
+
+
+    // Large Story
     try{
-        $sideStories = Get::all('articles', 7);
+        $largeStories = Get::all('articles', 1);
     }
     catch(Exeption $e){
         die("Exception: ".$e-> getMessage());
     }
 
-    // medium stories
+    // Medium stories
     try{
-        $medStories = Get::all('articles', 4);
+        $medStories = Get::all('articles', 6);
+    }
+    catch(Exeption $e){
+        die("Exception: ".$e-> getMessage());
+    }
+
+     // Small stories
+    try{
+        $sideStories = Get::all('articles', 13);
+        
+        // $sideStories = Get::byCategory('Economy');
     }
     catch(Exeption $e){
         die("Exception: ".$e-> getMessage());
@@ -28,7 +39,6 @@
         $formatDate = strtotime($elementId->date);
         return date('l, j F', $formatDate);
     }
-
     //----------------------------------
     // Returns category from Id
     //----------------------------------
@@ -37,7 +47,7 @@
             $categories = Get::byId('genres', $id);
             
             if($categories){
-                return $categories->genre; 
+                return $categories->name; 
             }
             else{
                 throw new Exception("Failed to retrieve genre!");  
@@ -52,7 +62,6 @@
     //----------------------------------
     function getAuthor($id){
         try{
-           
             $writerId = Get::byId('writers', $id);
             $nameArr = array(
                 $writerId->first_name,
@@ -85,31 +94,37 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- Page Icon -->
+    <script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
+    <link rel="icon" href="icon.svg">
     
 
-    <title>Document</title>
+    <title>World News</title>
 </head>
 <body>
     <div class="container">
     <!-- main headline -->
+    <?php foreach($largeStories as $largeStory){ ?>
         <div class="width-8 headline nested">
             <div class="width-12">
+                
                 <div class="banner">
-                    <div class="category"><p>HEALTH</p></div>
+                    <div class="category"><p><?= getCategory($largeStory->genre_id)?></p></div>
                 </div>
-                <h1>Children aged 12 to 15 to be offered booster jab</h1>
-                <h2>Stephen Donnelly said the move follows advice from the National Immunisation Advisory Committee</h2>
+                <h1><?= $largeStory->headline ?></h1>
+                <h2><?= $largeStory->summary ?></h2>
                 <div class="nameDate">
                     <div class="name">
-                        <p>by Stephan McGhee &nbsp;•&nbsp;</p>
+                        <p>by <?= getAuthor($largeStory->id) ?> &nbsp;•&nbsp;</p>
                     </div>
                     <div class="date">
-                        <p>Thursday, 3 March</p>
+                        <p><?= setDate($largeStory) ?></p>
                     </div>
                 </div>
-                <p>MPs will get a £2,200 pay rise from next month, the parliamentary spending watchdog has announced, sparking a backlash given the change will coincide with the national insurance hike and a growing “cost of living crisis”.</p>
-                <p>The 2.7% increase in MPs' salaries is nearly half the current rate of inflation, effectively meaning they will get a real-terms pay cut, but comes against a backdrop of significant economic hardship for many and the Bank of England urging workers not to ask for sizeable pay rises to try to stop prices spiralling out of control.</p>
+                <p><?= $largeStory->article ?></p>
+                
             </div>
+            <?php } ?>
         <!-- end main headline -->
         <!-- medium stories -->
         <?php
@@ -129,40 +144,6 @@
         <?php
             }
         ?>
-
-            <!-- <div class="width-6">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>Ukraine crisis: Joe Biden accuses Russia of running ‘false flag operation’ to justify invasion</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-                <p>MPs will get a £2,200 pay rise from next month, the parliamentary spending watchdog has announced, sparking a backlash given the change will coincide with the national insurance hike and a growing “cost of living crisis”.</p>
-                <p>The 2.7% increase in MPs' salaries is nearly half the current rate of inflation, effectively meaning they will get a real-terms pay cut, but comes against a backdrop of significant economic hardship for many and the Bank of England urging workers not to ask for sizeable pay rises to try to stop prices spiralling out of control.</p>
-            </div>
-            <div class="width-6">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>Ukraine crisis: Joe Biden accuses Russia of running ‘false flag operation’ to justify invasion</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-                <p>MPs will get a £2,200 pay rise from next month, the parliamentary spending watchdog has announced, sparking a backlash given the change will coincide with the national insurance hike and a growing “cost of living crisis”.</p>
-                <p>The 2.7% increase in MPs' salaries is nearly half the current rate of inflation, effectively meaning they will get a real-terms pay cut, but comes against a backdrop of significant economic hardship for many and the Bank of England urging workers not to ask for sizeable pay rises to try to stop prices spiralling out of control.</p>
-            </div>
-            <div class="width-6">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>Ukraine crisis: Joe Biden accuses Russia of running ‘false flag operation’ to justify invasion</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-                <p>MPs will get a £2,200 pay rise from next month, the parliamentary spending watchdog has announced, sparking a backlash given the change will coincide with the national insurance hike and a growing “cost of living crisis”.</p>
-                <p>The 2.7% increase in MPs' salaries is nearly half the current rate of inflation, effectively meaning they will get a real-terms pay cut, but comes against a backdrop of significant economic hardship for many and the Bank of England urging workers not to ask for sizeable pay rises to try to stop prices spiralling out of control.</p>
-            </div> -->
         </div>
         <!-- end medium stories -->
         <!-- sidebar -->
@@ -185,71 +166,6 @@
             <?php
                 }
             ?>
-            <!-- <div class="width-12">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>'Constant shelling' as Russian forces lay siege to key Ukrainian cities</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-            </div> 
-            <div class="width-12">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>'Constant shelling' as Russian forces lay siege to key Ukrainian cities</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-            </div> 
-            <div class="width-12">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>'Constant shelling' as Russian forces lay siege to key Ukrainian cities</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-            </div> 
-            <div class="width-12">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>'Constant shelling' as Russian forces lay siege to key Ukrainian cities</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-            </div> 
-            <div class="width-12">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>UK and New Zealand sign free trade deal</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-            </div> 
-            <div class="width-12">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>People of colour fleeing Ukraine attacked by Polish nationalists</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-            </div> 
-            <div class="width-12">
-                <div class="banner">
-                    <div class="category"><p>POLITICS</p></div>
-                </div>
-                <h3>EU carbon permit prices crash after Russian invasion of Ukraine</h3>
-                <div class="date">
-                    <p>Thursday, 3 March</p>
-                </div>
-            </div>  -->
-
-
         </div>
     </div>
    
